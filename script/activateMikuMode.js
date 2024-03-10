f9d6e6a0a5a42c49dc7d0192da53de92959248a7
CommandTM <commandnoobmc23@gmail.com>
2024-03-09 22:17:38 -0500

    Miku Mode Is Now Cookies

--- script/activateMikuMode.js
+++ script/activateMikuMode.js
@@ -1,5 +1,10 @@
+if (document.cookie === "miku=yes"){
+    activateMikuMode()
+}
+
 function activateMikuMode(){
-    document.getElementById("head").innerHTML += "<link rel=\"stylesheet\" href=\"style/mikuMode.css\"/>"
+    document.head.innerHTML += "<link rel=\"stylesheet\" href=\"style/mikuMode.css\"/>"
     document.getElementById("mikuModeButton").hidden = true
     document.getElementById("introHeader").innerText = document.getElementById("introHeader").innerText.replace("Command", "Hatsune Miku")
+    document.cookie = "miku=yes"
 }

6d92038a2e74f8e258aa1d1104391247ae40640e
colin-every <commandnoobmc23@gmail.com>
2024-03-19 10:12:58 -0400

    Hacking Implemented And Saving!

--- script/gameScripts/main.js
+++ script/gameScripts/main.js
@@ -12,8 +12,10 @@ const amountPrice = 100
 const multPrice = 1000
 const workerPrice = 10000
 const intervalPrice = 100000
+const hackingPrice = 100000000000
 const severityPrice = 10
 const impactPrice = 20
+const optimizePrice = 100
 
 const progressDisplay = document.getElementById("hackProgress")
 
@@ -22,8 +24,10 @@ var workInterval
 var hackProgress = 0
 var hackInterval
 
-//unlock("hSeverity")
-//unlock("hImpact")
+/*
+money = hackingPrice
+purchaseHacking()
+ */
 
 function test(){
     console.log("Click!")
@@ -96,7 +100,22 @@ function purcahseInterval(){
 
         if (interval <= 0.001){
             lock("interval")
+            unlock("hacking")
         }
+    }
+}
+
+function purchaseHacking(){
+    if (money >= hackingPrice){
+        lock("hacking")
+        money = money - hackingPrice
+        unlock("hSeverity")
+        unlock("hImpact")
+        unlock("hOptimize")
+
+        document.getElementById("hSeverityDisplay").hidden = false
+        document.getElementById("hImpactDisplay").hidden = false
+        document.getElementById("mainHackWindow").hidden = false
     }
 }
 
@@ -113,6 +132,13 @@ function purchaseImpact(){
         pounds -= impactPrice
         progressDisplay.max = Math.round(progressDisplay.max*1.1)
         impact++
+    }
+}
+
+function purchaseOptimization(){
+    if (pounds >= optimizePrice && progressDisplay.max > 1){
+        pounds -= optimizePrice
+        progressDisplay.max -= 1
     }
 }
 
@@ -140,6 +166,84 @@ function cheat(type){
     }
 }
 
+function save(){
+    localStorage.setItem("exists", "yes")
+    localStorage.setItem("version", 1)
+    localStorage.setItem("money", money)
+    localStorage.setItem("amount", amount)
+    localStorage.setItem("multUnlocked", !document.getElementById("multText").hidden)
+    localStorage.setItem("mult", mult)
+    localStorage.setItem("workerUnlocked", !document.getElementById("workerText").hidden)
+    localStorage.setItem("workers", workers)
+    localStorage.setItem("intervalUnlocked", !document.getElementById("intervalText").hidden)
+    localStorage.setItem("interval", interval)
+    localStorage.setItem("hackingPurchasable", !document.getElementById("hackingText").hidden)
+    localStorage.setItem("hackingUnlocked", !document.getElementById("mainHackWindow").hidden)
+    localStorage.setItem("pounds", pounds)
+    localStorage.setItem("severity", severity)
+    localStorage.setItem("impact", impact)
+    localStorage.setItem("optimization", progressDisplay.max)
+    localStorage.setItem("cheating", !document.getElementById("cheatWindow").hidden)
+}
+
+function clearSave(){
+    localStorage.setItem("exists", null)
+    location.reload()
+}
+
+function load(){
+    if (localStorage.getItem("exists") === "yes"){
+        money = Number(localStorage.getItem("money"))
+        amount = parseInt(localStorage.getItem("amount"))
+        mult = parseInt(localStorage.getItem("mult"))
+        workers = parseInt(localStorage.getItem("workers"))
+        interval = parseFloat(localStorage.getItem("interval"))
+        pounds = parseInt(localStorage.getItem("pounds"))
+        severity = parseInt(localStorage.getItem("severity"))
+        impact = parseInt(localStorage.getItem("impact"))
+        progressDisplay.max = parseInt(localStorage.getItem("optimization"))
+
+        if (localStorage.getItem("multUnlocked") === "true"){
+            unlock("mult")
+        }
+
+        if (localStorage.getItem("workerUnlocked") === "true"){
+            unlock("worker")
+        }
+
+        if (localStorage.getItem("intervalUnlocked") === "true"){
+            unlock("interval")
+        }
+
+        if (localStorage.getItem("hackingPurchasable") === "true"){
+            unlock("hacking")
+        }
+
+        if (localStorage.getItem("hackingUnlocked") === "true"){
+            money += hackingPrice
+            purchaseHacking()
+        }
+
+        if (localStorage.getItem("cheating") === "true"){
+            document.getElementById("cheatWindow").hidden = false
+        }
+
+        if (workers >= 1){
+            workInterval = setInterval(work, (1000))
+        }
+
+        if (interval < 1){
+            document.getElementById("intervalDisplay").style.width = (300*interval) + "px"
+            clearInterval(workInterval)
+            workInterval = setInterval(work, (1000*interval))
+        }
+
+        for (let i = 0; i < workers; i++){
+            document.getElementById("workerDisplay").innerText += "☺"
+        }
+    }
+}
+
 setInterval(display, 1)
 
 const buttonWindow = new Window("buttonWindow");
@@ -147,3 +251,7 @@ const moneyWindow = new Window("moneyWindow")
 const storeWindow = new Window("storeWindow")
 const hackWindow = new Window("mainHackWindow")
 const cheatWindow = new Window("cheatWindow")
+const contractsWindow = new Window("hackContractsWindow")
+const saveWindow = new Window("saveWindow")
+
+load()
